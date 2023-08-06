07eae27bab8388d39e5233d165e5407faf18abe9
Natasha Wilhelm <ewilhelm31@gmail.com>
2023-08-06 15:10:29 -0400

    changes

--- alien.py
+++ alien.py
@@ -1,7 +1,20 @@
-alien_0 = {'color': 'green', 'points' : 5}
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x_position: " + str(alien_0['x_position']))
 
-print(alien_0['color'])
-print(alien_0['points'])
+# Move the alien to the right
+# Determine how far to move the alien based on its current speed.
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # This must be fast alien.
+    x_increment = 3
+
+# The new position is the old position plus new increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print("New x-position: " + str(alien_0['x_position']))
 
 
 
