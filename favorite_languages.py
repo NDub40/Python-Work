48a38707a0ebad052b83ed5c09b87af5125eb3c2
Natasha Wilhelm <ewilhelm31@gmail.com>
2023-08-08 10:15:53 -0400

    changes

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,7 +5,12 @@ favorite_languages = {
     'phil': 'python',
     }
 
-for name, language in favorite_languages.items():
-    print(name.title() + "'s favorite language is " +
-          language.title() + ".")
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
 
+    if name in friends:
+        print("  Hi " + name.title() +
+              ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+              
